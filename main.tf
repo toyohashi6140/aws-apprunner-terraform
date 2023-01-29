79081fcf408c1b96cb84405ea86ce8e4b570e75b
DESKTOP-73796H6\karum <karumagt7@outlook.jp>
2023-01-29 15:58:12 +0900

    create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "resion" {
+    type = string
+    nullable = false
+    default = "ap-northeast-1"
+}
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  resion = var.resion
+}
+
+resource "aws_ecr_repository" "default" {
+  name = "default-repository"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
