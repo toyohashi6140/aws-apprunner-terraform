c80c1c8b96b423fc2b0cafa7e35b745f6c61475f
DESKTOP-73796H6\karum <karumagt7@outlook.jp>
2023-01-29 19:16:25 +0900

    update fix spell miss

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "resion" {
+variable "region" {
     type = string
     nullable = false
     default = "ap-northeast-1"
@@ -9,7 +9,7 @@ variable "resion" {
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  resion = var.resion
+  region = var.region
 }
 
 resource "aws_ecr_repository" "default" {
